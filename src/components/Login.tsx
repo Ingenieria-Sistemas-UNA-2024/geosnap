4184e5e92df47145794ff27cb9a333c177141dfa
Ghost-Red <raulbejarano2002@gmail.com>
2024-06-10 02:34:26 -0600

    Se hicieron arreglos a la interfas de login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,158 +1,155 @@
-import React, { useState } from 'react';
-import { Alert, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from 'react-native';
-import { Camera  } from 'lucide-react-native';
-
-import { StackNavigationProp } from '@react-navigation/stack';
+import Input from "./Input"
+import signInUser from "../libs/signInUser"
+import React, { useRef } from "react"
+import { theme } from "../utils/theme"
+import { Camera } from "lucide-react-native"
+import { StackNavigationProp } from "@react-navigation/stack"
+import { Alert, Pressable, SafeAreaView, StyleSheet, Text, TextInput, View } from "react-native"
 
 type RootStackParamList = {
-  Registro: undefined;
-  BottomNavigation: undefined;
-};
+  Registro: undefined
+  BottomNavigation: undefined
+}
 
-type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, 'Registro'>;
+type LoginScreenNavigationProp = StackNavigationProp<RootStackParamList, "Registro">
 
 interface LoginProps {
-  navigation: LoginScreenNavigationProp;
+  navigation: LoginScreenNavigationProp
 }
 
 export default function Login({ navigation }: LoginProps): JSX.Element {
-    const [username, setUsername] = useState("");
-    const [password, setPassword] = useState("");
+  const email = useRef<string>("")
+  const password = useRef<string>("")
 
-    const handleSignUpPress = () => {
-        navigation.navigate('Registro');
-    };
+  const handleSignInPress = () => {
+    if (email.current === "" || password.current === "") {
+      Alert.alert("Todos los campos son obligatorios")
+      return
+    }
+    try {
+      signInUser(email.current, password.current)
+      navigation.navigate("TabNavigator")
+    } catch (error) {
+      Alert.alert("Ocurrio un error al iniciar sesión")
+    }
+  }
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.logoContainer}>
-                <Camera  color="#54377f" size={100} />
-                <Text style={styles.title}>Geo-Snap</Text>
-            </View>
-            <View style={styles.formContainer}>
-                <View style={styles.inputView}>
-                    <Text style={styles.inputDescriptionText}>Correo Electrónico</Text>
-                    <TextInput
-                        style={styles.input}
-                        placeholder=''
-                        placeholderTextColor="#868e94"
-                        value={username}
-                        onChangeText={setUsername}
-                        autoCorrect={false}
-                        autoCapitalize='none'
-                    />
-                </View>
-                <View style={styles.inputView}>
-                    <View style={styles.passwordRow}>
-                        <Text style={styles.passwordText}>Contraseña</Text>
-                        <Pressable onPress={() => Alert.alert("Módulo en mantenimiento")}>
-                            <Text style={styles.forgetText}>¿Has olvidado la contraseña?</Text>
-                        </Pressable>
-                    </View>
-                    <TextInput
-                        style={styles.input}
-                        placeholder=''
-                        placeholderTextColor="#868e94"
-                        secureTextEntry
-                        value={password}
-                        onChangeText={setPassword}
-                        autoCorrect={false}
-                        autoCapitalize='none'
-                    />
-                </View>
-                <View style={styles.buttonView}>
-                    <Pressable style={styles.button} onPress={() => navigation.navigate('TabNavigator')}>
-                        <Text style={styles.buttonText}>Iniciar Sesión</Text>
-                    </Pressable>
-                </View>
-                <Text style={styles.footerText}>
-                    No tiene cuenta?
-                    <Text style={styles.signup} onPress={handleSignUpPress}>  Registrarse</Text>
-                </Text>
-            </View>
-        </SafeAreaView>
-    );
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.logoContainer}>
+        <Camera color="#54377f" size={100} />
+        <Text style={styles.title}>Geo-Snap</Text>
+      </View>
+      <View style={{ width: "100%", gap: 20 }}>
+        <Input
+          label="Correo Electrónico"
+          onChangeText={(value) => {
+            email.current = value
+          }}
+          autoCapitalize="none"
+        />
+        <Input
+          label="Contraseña"
+          onChangeText={(value) => {
+            password.current = value
+          }}
+          secureTextEntry
+          autoCapitalize="none"
+        />
+        <View style={{ alignItems: "center" }}>
+          <Pressable style={styles.button} onPress={() => handleSignInPress}>
+            <Text style={styles.buttonText}>Iniciar Sesión</Text>
+          </Pressable>
+        </View>
+        <Text style={styles.footerText}>
+          No tiene cuenta?
+          <Text style={styles.signup} onPress={() => navigation.navigate("Registro")}>
+            {" "}
+            Registrarse
+          </Text>
+        </Text>
+        <Text style={styles.footerText}>
+          ¿Has olvidado la contraseña?
+          <Text style={styles.signup} onPress={() => Alert.alert("Módulo en mantenimiento")}>
+            {" "}
+            Recuperar
+          </Text>
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#161B22',
-    justifyContent: 'center',
-    alignItems: 'center',
+    backgroundColor: theme.colors.backgroundSecundary,
+    justifyContent: "center",
+    alignItems: "center",
   },
   logoContainer: {
-    alignItems: 'center',
+    alignItems: "center",
     marginBottom: 40,
   },
   title: {
     fontSize: 35,
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    textAlign: 'center',
-    color: '#54377f',
+    fontWeight: "bold",
+    textTransform: "uppercase",
+    textAlign: "center",
+    color: theme.colors.primary,
     marginTop: 20,
-  },
-  formContainer: {
-    width: '80%',
-  },
-  inputView: {
-    marginBottom: 20,
   },
   inputDescriptionText: {
     fontSize: 14,
-    color: '#868e94',
+    color: "#868e94",
     marginBottom: 5,
   },
   passwordRow: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
   },
   passwordText: {
     fontSize: 14,
-    color: '#868e94',
+    color: theme.colors.textSecundary,
   },
   forgetText: {
     fontSize: 11,
-    color: '#3366bb',
+    color: "#3366bb",
     marginVertical: 6,
-    textAlign: 'right',
+    textAlign: "right",
   },
   input: {
     height: 50,
-    width: '100%',
+    width: "100%",
     paddingHorizontal: 20,
-    borderColor: '#868e94',
+    borderColor: theme.colors.textSecundary,
     borderWidth: 1,
     borderRadius: 7,
-    color: '#feffff',
-  },
-  buttonView: {
-    alignItems: 'center',
+    color: theme.colors.white,
   },
   button: {
-    backgroundColor: '#54377f',
+    backgroundColor: "#54377f",
     height: 55,
-    width: '100%',
-    borderColor: 'gray',
+    width: "100%",
+    borderColor: "gray",
     borderWidth: 1,
     borderRadius: 5,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   buttonText: {
-    color: 'white',
-    fontSize: 14,
-    fontWeight: 'bold',
+    color: "white",
+    fontSize: theme.fontSizes.button,
+    fontWeight: "bold",
     margin: 10,
   },
   footerText: {
-    textAlign: 'center',
-    color: 'gray',
+    textAlign: "center",
+    color: "gray",
     marginTop: 30,
   },
   signup: {
-    color: '#3366bb',
-    fontSize: 13,
-  }
-});
+    color: "#3366bb",
+    fontSize: theme.fontSizes.button,
+  },
+})
---
+++ src/libs/signInUser.ts
@@ -0,0 +1,6 @@
+
+const signInUser = async(email: string, password: string): Promise<User> => {
+  return {} as User
+}
+
+export default signInUser
